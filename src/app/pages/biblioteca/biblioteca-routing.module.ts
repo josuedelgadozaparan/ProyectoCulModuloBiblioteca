c17bdfaf64fccf47ead6ef54a4d5adce662afc1c
josuedelgadozaparan <josuedelgadozaparan@gmail.com>
2019-09-22 15:11:40 -0500

    Actividad -  mudulo biblioteca

---
+++ src/app/pages/biblioteca/biblioteca-routing.module.ts
@@ -0,0 +1,43 @@
+import { TipoComponent } from './tipo/tipo.component';
+import { LibroComponent } from './libro/libro.component';
+import { ExistenciaComponent } from './existencia/existencia.component';
+import { EditorialComponent } from './editorial/editorial.component';
+import { AutoresComponent } from './autores/autores.component';
+import { AreasComponent } from './areas/areas.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+
+const routes: Routes = [
+  {
+    path: 'areas',
+    component: AreasComponent
+  },
+  {
+    path: 'autores',
+    component: AutoresComponent
+  },
+  {
+    path: 'editorial',
+    component: EditorialComponent
+  },
+  {
+    path: 'existencia',
+    component: ExistenciaComponent
+  },
+  {
+    path: 'libro',
+    component: LibroComponent
+  },
+  {
+    path: 'tipo',
+    component: TipoComponent
+  }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class BibliotecaRoutingModule { }
